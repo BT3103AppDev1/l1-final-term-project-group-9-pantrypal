29b1eb4965087d05755d50fb188237e8470bc342
Kay Eugenia Purnama <122983473+kayeugenia@users.noreply.github.com>
2024-03-18 17:21:33 +0800

    fix(recipe-generator): fix chefbot context

--- chatgpt-backend/chatGpt.controller.js
+++ chatgpt-backend/chatGpt.controller.js
@@ -2,39 +2,39 @@ const { OpenAI } = require("openai");
 require("dotenv").config();
 
 const askToChatGpt = async function (req, res) {
-  /**
-   * 1. Create/configure OpenAI Instance
-   */
-  const openai = _createOpenAIInstance();
-
-  /**
-   * 2. Let's talk to chatGPT
-   */
-  try {
-    const completion = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo-0125",
-        messages: [
-            { 
-                role: "user", 
-                content: req.body.message 
-            }],
-    });
-    const repliedMessage = completion.choices[0].message.content;
-    res.send({ from: "chatGpt", data: repliedMessage });
-  } catch (error) {
-    // Report error
-    console.log("Error ", error);
-    res.status(500).send({ error: "Internal Server Error" });
-  }
-};
+    const openai = _createOpenAIInstance();
+  
+    try {
+        const conversationHistory = req.body.conversationHistory || [];
+    
+        conversationHistory.push({ role: "user", content: req.body.message });
+    
+        const completion = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo-0125",
+            messages: conversationHistory,
+        });
+    
+        const repliedMessage = completion.choices[0].message.content;
+    
+        res.send({ 
+            from: "chatGpt", 
+            data: repliedMessage, 
+            conversationHistory: conversationHistory 
+        });
+        } catch (error) {
+        console.log("Error ", error);
+        res.status(500).send({ error: "Internal Server Error" });
+        }
+  };
+  
 
 const _createOpenAIInstance = () => {
-  const openai = new OpenAI({
-    apiKey: process.env.CHATGPT_TOKEN,
-  });
-  return openai;
+    const openai = new OpenAI({
+        apiKey: process.env.CHATGPT_TOKEN,
+    });
+    return openai;
 };
 
 module.exports = {
-  askToChatGpt,
+    askToChatGpt,
 };
