b9f3e404192e595c58a42d7b06b40ccabf1ee0aa
Yuen Ning <e0970430@u.nus.edu>
2024-03-19 14:57:10 +0800

    feat(community-page): extract recipe card and recipe details window as components, and update recipe cards css

--- pantrypal-frontend/src/components/RecipeCard.vue
+++ pantrypal-frontend/src/components/RecipeCard.vue
@@ -1,21 +1,36 @@
 <template>
-  <div class="recipe-card">
-    <h2>{{ recipe.title }}</h2>
-    <img :src="recipe.image" :alt="recipe.title" />
-    <p>{{ recipe.description }}</p>
-    <button @click="viewRecipe">View Recipe</button>
+  <div class="recipe-card" @click="toggleRecipeDetails">
+    <div class="recipe-image">
+      <img :src="recipe.recipe_img_url" :alt="recipe.recipe_name" />
+    </div>
+    <div class="recipe-details">
+      <h2>{{ recipe.recipe_name }}</h2>
+      <div class="info">
+        <span
+          v-for="(category, index) in recipe.categories"
+          :key="index"
+          class="category-bubble"
+          >{{ category }}</span
+        >
+      </div>
+      <div class="user-id">
+        <p>@{{ recipe.user_id }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   props: {
-    recipe: Object,
+    recipe: {
+      type: Object,
+      required: true,
+    },
   },
   methods: {
-    viewRecipe() {
-      // Add your logic to navigate to the recipe details page
-      console.log("Viewing recipe:", this.recipe.title);
+    toggleRecipeDetails() {
+      this.$emit("toggle", this.recipe);
     },
   },
 };
@@ -23,15 +38,63 @@ export default {
 
 <style scoped>
 .recipe-card {
+  width: calc(33.33% - 20px);
+  height: 40vh;
   border: 1px solid #ccc;
   border-radius: 8px;
-  padding: 20px;
-  margin: 10px;
+  overflow: hidden;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  transition: transform 0.3s ease;
+  line-height: 1;
+  display: flex;
+  flex-direction: column;
 }
 
-.recipe-card img {
+.recipe-card:hover {
+  transform: scale(1.02);
+  transition: transform 0.3s ease;
+}
+
+.recipe-image {
+  flex: 1;
+  overflow: hidden;
+}
+
+.recipe-image img {
   width: 100%;
-  max-height: 200px;
+  height: 100%;
   object-fit: cover;
+}
+
+.recipe-details {
+  padding: 15px;
+  background-color: #cbdf99;
+  flex: 0.5;
+}
+
+.recipe-details h2,
+.recipe-details .info,
+.recipe-details p {
+  margin-bottom: 5px;
+}
+
+.info {
+  display: flex;
+  justify-content: space-between;
+}
+
+.category-bubble {
+  display: inline-block;
+  background-color: #fffce2;
+  color: #333;
+  padding: 5px 10px;
+  border-radius: 20px;
+  margin-right: 5px;
+  font-size: 0.75rem;
+}
+
+.user-id {
+  font-style: italic;
+  font-size: 0.75rem;
 }
 </style>
