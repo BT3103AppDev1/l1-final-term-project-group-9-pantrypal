0d83ba0947bf8afb43d8e36a9220a846349c7276
poon-zx <poonzhexuan@gmail.com>
2024-03-24 11:25:07 +0800

    feat(not-found) add NotFound page

---
+++ pantrypal-frontend/src/views/NotFound.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="app">
+      <div class="top-bar">
+        <img src="../assets/logo.jpg" alt="PantryPal Logo" class="logo">
+      </div>
+      <div class="separator"></div>
+      <div class="gallery-container">
+        <header>
+            <h1>404 Error: Page Not Found</h1>
+        </header>
+        <div>
+            <p>The above link does not exist, click below to go back to landing page.</p>
+            <button class="btn-login" @click="goToLanding">Landing Page</button>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NotFoundPage',
+    methods: {
+        goToLanding() {
+        this.$router.push('/');
+        },
+    }
+};
+  </script>
+  
+  <style scoped>
+  @import "@/styles/landing-page.css";
+  </style>
+  
