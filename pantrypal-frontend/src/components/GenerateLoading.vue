56714d069ff548c2aeb0f296751d9a14d7514ecd
madhupolluru <129572044+madhupolluru@users.noreply.github.com>
2024-04-01 15:29:53 +0800

    minor code formatting edits

--- pantrypal-frontend/src/components/GenerateLoading.vue
+++ pantrypal-frontend/src/components/GenerateLoading.vue
@@ -1,54 +1,73 @@
 <template>
-<div class="generate-container">
+  <div class="generate-container">
     <h1>We are cooking up your recipe!</h1>
     <h1>Give us a few seconds...</h1>
     <div v-for="(ingredient, index) in ingredients">
-        <div class="ingredient-container">
-            <span>
-                {{ingredients[index].name}}
-            </span>
-            <span>
-                {{ingredients[index].quantity}}
-            </span>
-        </div>
+      <div class="ingredient-container">
+        <span>
+          {{ ingredients[index].name }}
+        </span>
+        <span>
+          {{ ingredients[index].quantity }}
+        </span>
+      </div>
     </div>
-    <img src="@/assets/loading.gif" height="40px"/>
-</div>
+    <img src="@/assets/loading.gif" height="40px" />
+  </div>
 </template>
 
+
 <script>
+import axios from "axios";
+
 export default {
-    name: 'Generator Loading',
-    props: {
-        ingredients: [],
-    },
+  name: "Generator Loading",
+  props: {
+    ingredients: Array,
+    categories: Array,
+    dietaryRestrictions: String,
+  },
+  async mounted() {
+    console.log("mounted..");
+    await axios
+      .post("http://localhost:3000/initial-recipe", {
+        ingredients: this.ingredients,
+        categories: this.categories,
+        dietaryRestrictions: this.dietaryRestrictions,
+      })
+      .then((response) => {
+        this.$emit("recipeGenerated", {
+          generatedRecipe: response.data.content,
+        });
+      });
+  },
 };
 </script>
 
 <style scoped>
-    h1 {
-        margin: 0;
-    }
+h1 {
+  margin: 0;
+}
 
-    .generate-container {
-        padding: 8rem 0rem;
-        text-align: center;
-        width: 600px;
-    }
+.generate-container {
+  padding: 8rem 0rem;
+  text-align: center;
+  width: 600px;
+}
 
-    .ingredient-container {
-        display: flex;
-        justify-content: space-between;
-        margin: 1rem;
-    }
+.ingredient-container {
+  display: flex;
+  justify-content: space-between;
+  margin: 1rem;
+}
 
-    .ingredient-container {
-        background-color: #ECECEC;
-        padding: 1rem 2rem;
-        border-radius: 10px;
-    }
+.ingredient-container {
+  background-color: #ececec;
+  padding: 1rem 2rem;
+  border-radius: 10px;
+}
 
-    img {
-        padding: 2rem;
-    }
+img {
+  padding: 2rem;
+}
 </style>
--- pantrypal-frontend/src/components/ProfileSidebar.vue
+++ pantrypal-frontend/src/components/ProfileSidebar.vue
@@ -1,130 +1,131 @@
 <template>
-    <aside class="sidebar">
-        <div class="profile-container">
-            <div class="profile-pic">
-                <img src="../assets/profile.svg" alt="profile pic" class="profile" />
-            </div>
-            <div class="personal-details">
-                <h4 class="user-name">@{{ username }}</h4>
-                <h4 class="created-number">Created {{ created_count }} recipes</h4>
-            </div>
-            <div class="profile-options">
-                <div class="header setting">
-                    <h3>Settings</h3>
-                </div>
-                <div class="header" @click="goToLikedPage()">
-                    <h3>Liked Recipes</h3>
-                </div>
-                <div class="header" @click="goToCookbookPage()">
-                    <h3>My Cookbook</h3>
-                </div>
-            </div>
+  <aside class="sidebar">
+    <div class="profile-container">
+      <div class="profile-pic">
+        <img src="../assets/profile.svg" alt="profile pic" class="profile" />
+      </div>
+      <div class="personal-details">
+        <h4 class="user-name">@{{ username }}</h4>
+        <h4 class="created-number">Created {{ created_count }} recipes</h4>
+      </div>
+      <div class="profile-options">
+        <div class="header setting">
+          <h3>Settings</h3>
         </div>
-    </aside>
+        <div class="header" @click="goToLikedPage()">
+          <h3>Liked Recipes</h3>
+        </div>
+        <div class="header" @click="goToCookbookPage()">
+          <h3>My Cookbook</h3>
+        </div>
+      </div>
+    </div>
+  </aside>
 </template>
 <script>
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getFirestore, doc, getDoc } from "firebase/firestore";
 
 export default {
-    name: 'Profile Sidebar',
-    mounted() {
-        this.fetchUserData();
-    },
-    data() {
-        return {
-            username:'',
-            created_count:0
+  name: "Profile Sidebar",
+  mounted() {
+    this.fetchUserData();
+  },
+  data() {
+    return {
+      username: "",
+      created_count: 0,
+    };
+  },
+  methods: {
+    async fetchUserData() {
+      const auth = getAuth();
+      onAuthStateChanged(auth, async (user) => {
+        if (user) {
+          const db = getFirestore();
+          const userRef = doc(db, "users", user.uid);
+          const userSnap = await getDoc(userRef);
+          if (userSnap.exists()) {
+            this.username = userSnap.data().username;
+            this.created_count = userSnap.data().my_cookbook.length;
+          }
+        } else {
+          console.log("No such user document");
         }
-    }, 
-    methods: {
-        async fetchUserData() {
-            const auth = getAuth();
-            onAuthStateChanged(auth, async (user) => {
-                if (user) {
-                    const db = getFirestore();
-                    const userRef = doc(db, 'users', user.uid);
-                    const userSnap = await getDoc(userRef);
-                    if (userSnap.exists()) {
-                        this.username = userSnap.data().username;
-                        this.created_count = userSnap.data().my_cookbook.length;
-                    }
-                } else {
-                    console.log('No such user document');
-                }
-            });
-        },
-        goToLikedPage() {
-            this.$router.push('/profile/liked');
-        },
-        goToCookbookPage() {
-            this.$router.push('/profile/cookbook');
-        },
-    }
+      });
+    },
+    goToLikedPage() {
+      this.$router.push("/profile/liked");
+    },
+    goToCookbookPage() {
+      this.$router.push("/profile/cookbook");
+    },
+  },
 };
 </script>
 
 <style scoped>
 .sidebar {
-    background-color: #3C1F11;
-    min-width: 12rem;
-    padding: 1rem;
-    display: flex;
-    justify-content: center;
-    text-align: center;
-    position: sticky;
-    height: 100vh;
-    top: 0;
-    overflow-y: hidden;
+  background-color: #3c1f11;
+  min-width: 12rem;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  position: sticky;
+  height: 100vh;
+  top: 0;
+  overflow-y: hidden;
 }
 
 .profile-container {
-    padding-top: 2rem; 
-    height: fit-content;
+  padding-top: 2rem;
+  height: fit-content;
 }
 
 .profile-pic img {
-    width: 120px; 
-    height: 120px; 
-    border-radius: 60px; 
-    margin-bottom: 2rem; 
+  width: 120px;
+  height: 120px;
+  border-radius: 60px;
+  margin-bottom: 2rem;
 }
 
 .personal-details {
-    margin-bottom: 3rem; 
+  margin-bottom: 3rem;
 }
 
 .header {
-    cursor: pointer; 
-    margin-bottom: 2rem; 
-    transition: background-color 0.3s, color 0.3s; 
+  cursor: pointer;
+  margin-bottom: 2rem;
+  transition: background-color 0.3s, color 0.3s;
 }
 
 .header:hover {
-    background-color: #A7BF6A; 
-    border-radius: 16px; 
-    color: #3C1F11;
+  background-color: #a7bf6a;
+  border-radius: 16px;
+  color: #3c1f11;
 }
 
 h3 {
-    color: white;
-    margin: 0 auto;
-    padding: 5px 0px;
+  color: white;
+  margin: 0 auto;
+  padding: 5px 0px;
 }
 
 .user-name {
-    color: #A7BF6A
+  color: #a7bf6a;
 }
 
 .created-number {
-    color: white
+  color: white;
 }
 
-.selected h3, .setting {
-    background-color: #A7BF6A; 
-    width: 100%;
-    padding: 5px 5px;
-    border-radius: 16px;
-    color: #3C1F11;
+.selected h3,
+.setting {
+  background-color: #a7bf6a;
+  width: 100%;
+  padding: 5px 5px;
+  border-radius: 16px;
+  color: #3c1f11;
 }
 </style>
