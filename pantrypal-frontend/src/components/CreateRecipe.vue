0e218fab5e7a67d61ba88f8ae07d1cc495e4327e
Yuen Ning <e0970430@u.nus.edu>
2024-04-04 22:29:14 +0800

    feat(liked-recipes): implement auto refresh

--- pantrypal-frontend/src/components/CreateRecipe.vue
+++ pantrypal-frontend/src/components/CreateRecipe.vue
@@ -39,23 +39,13 @@
           </div>
           <div class="first2">
             <label for="recipeName">Title:</label>
-            <input
-              type="text"
-              id="recipeName"
-              v-model="recipeData.recipe_name"
-            />
+            <input type="text" id="recipeName" v-model="recipeData.recipe_name" />
 
             <label for="recipeDescription">Description:</label>
-            <textarea
-              id="recipeDescription"
-              v-model="recipeData.description"
-            ></textarea>
+            <textarea id="recipeDescription" v-model="recipeData.description"></textarea>
 
             <label for="allergenInfo">Allergen Information:</label>
-            <textarea
-              id="allergenInfo"
-              v-model="recipeData.allergen_info"
-            ></textarea>
+            <textarea id="allergenInfo" v-model="recipeData.allergen_info"></textarea>
 
             <!-- Added Cook Time, Category and Serving Size -->
             <div class="additional-info">
@@ -115,10 +105,7 @@
                     v-model="recipeData.ingredients[index]"
                     placeholder="e.g. 10g Apple"
                   />
-                  <button
-                    class="remove-button"
-                    @click="removeIngredient(index)"
-                  >
+                  <button class="remove-button" @click="removeIngredient(index)">
                     x
                   </button>
                 </div>
@@ -149,10 +136,7 @@
                       :id="'direction' + direction.stepNumber"
                       v-model="direction.text"
                     ></textarea>
-                    <button
-                      class="remove-button"
-                      @click="removeDirection(index)"
-                    >
+                    <button class="remove-button" @click="removeDirection(index)">
                       x
                     </button>
                   </div>
@@ -187,12 +171,7 @@ import {
   fetchCategories,
 } from "../firebase.js";
 import { ref as storageRef } from "firebase/storage";
-import {
-  getFirestore,
-  addDoc,
-  collection,
-  updateDoc,
-} from "firebase/firestore";
+import { getFirestore, addDoc, collection, updateDoc } from "firebase/firestore";
 
 export default {
   components: {
@@ -297,9 +276,7 @@ export default {
       const userId = user ? user.uid : "";
 
       const recipe = {
-        allergens: this.recipeData.allergen_info
-          .split(",")
-          .map((word) => word.trim()),
+        allergens: this.recipeData.allergen_info.split(",").map((word) => word.trim()),
         categories: this.recipeData.category,
         community: this.recipeData.publish_to_community,
         cook_time:
