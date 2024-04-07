41e388b7216fa439d806ad4d0551a43ba7ca5b1e
madhupolluru <129572044+madhupolluru@users.noreply.github.com>
2024-04-08 03:14:35 +0800

    enhance(creat/edit-recipe)

--- pantrypal-frontend/src/components/CircleButton.vue
+++ pantrypal-frontend/src/components/CircleButton.vue
@@ -1,39 +1,38 @@
 <template>
-    <div>
-        <button class="circle-button">
-            <img class="logo" :src=this.logo alt="button" />
-        </button> 
-    </div>
+  <div>
+    <button class="circle-button">
+      <img class="logo" :src="this.logo" alt="button" />
+    </button>
+  </div>
 </template>
-
 
 <script>
 export default {
-    name: 'Circle Button',
-    props: {
-        logo: String,
-    },
+  name: "Circle Button",
+  props: {
+    logo: String,
+  },
 };
 </script>
 
 <style scoped>
-    .circle-button {
-        width: 100px;
-        height: 100px;
-        border-radius: 50%;
-        background-color: #f5f5f5;
-        border: none;
-        box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
-        position: fixed;
-        bottom: 20px;
-        right: 20px;
-        background-color: #BD6B16;
-        cursor: pointer;
-        z-index: 1000;
-    }
+.circle-button {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  background-color: #f5f5f5;
+  border: none;
+  box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  background-color: #bd6b16;
+  cursor: pointer;
+  z-index: 1000;
+}
 
-    .logo {
-        width: 75%;
-        height: 75%;
-    }
+.logo {
+  width: 75%;
+  height: 75%;
+}
 </style>
