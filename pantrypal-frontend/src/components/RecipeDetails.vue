3c93b2bae0c99580a92e3fe3d600b0d5c2e579b5
Yuen Ning <e0970430@u.nus.edu>
2024-04-21 23:04:12 +0800

    fix(recipe-details): fix store issues

--- pantrypal-frontend/src/components/RecipeDetails.vue
+++ pantrypal-frontend/src/components/RecipeDetails.vue
@@ -24,56 +24,36 @@
         <h1>{{ selectedRecipe.recipe_name }}</h1>
         <LikeButton v-if="likeExists" :recipe="selectedRecipe" />
       </div>
-      <div
-        class="warning"
-        v-if="selectedRecipe.AIgenerated && selectedRecipe.editted"
-      >
-        <img
-          clas="warning-logo"
-          src="../assets/warning-icon.png"
-          height="16px"
-        />
+      <div class="warning" v-if="selectedRecipe.AIgenerated && selectedRecipe.editted">
+        <img clas="warning-logo" src="../assets/warning-icon.png" height="16px" />
         <span
-          >This recipe is AI-generated (edited) and PantryPal has not verified
-          it for accuracy and safety.</span
-        >
-      </div>
-      <div
-        class="warning"
-        v-if="selectedRecipe.AIgenerated && !selectedRecipe.editted"
-      >
-        <img
-          clas="warning-logo"
-          src="../assets/warning-icon.png"
-          height="16px"
-        />
-        <span
-          >This recipe is AI-generated and PantryPal has not verified it for
+          >This recipe is AI-generated (edited) and PantryPal has not verified it for
           accuracy and safety.</span
         >
       </div>
-      <div
-        class="warning"
-        v-if="!selectedRecipe.AIgenerated && selectedRecipe.editted"
-      >
-        <img
-          clas="warning-logo"
-          src="../assets/warning-icon.png"
-          height="16px"
-        />
+      <div class="warning" v-if="selectedRecipe.AIgenerated && !selectedRecipe.editted">
+        <img clas="warning-logo" src="../assets/warning-icon.png" height="16px" />
+        <span
+          >This recipe is AI-generated and PantryPal has not verified it for accuracy and
+          safety.</span
+        >
+      </div>
+      <div class="warning" v-if="!selectedRecipe.AIgenerated && selectedRecipe.editted">
+        <img clas="warning-logo" src="../assets/warning-icon.png" height="16px" />
         <span>This recipe has been edited since its previous revision.</span>
       </div>
       <p>
         <i v-if="likeExists">
           By @{{ username }},
           {{
-            new Date(
-              selectedRecipe.created_date.seconds * 1000
-            ).toLocaleDateString("en-GB", {
-              year: "numeric",
-              month: "long",
-              day: "numeric",
-            })
+            new Date(selectedRecipe.created_date.seconds * 1000).toLocaleDateString(
+              "en-GB",
+              {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              }
+            )
           }}
         </i>
         <i v-else>
@@ -96,10 +76,7 @@
       </p>
       <span class="allergens-container">
         <p><b>CONTAINS:</b></p>
-        <template
-          v-for="(allergen, index) in selectedRecipe.allergens"
-          :key="index"
-        >
+        <template v-for="(allergen, index) in selectedRecipe.allergens" :key="index">
           <span>{{ allergen }}</span>
           <p v-if="index < selectedRecipe.allergens.length - 1">,</p>
         </template>
@@ -119,10 +96,7 @@
       <div class="recipe-section">
         <h3>Ingredients:</h3>
         <ul class="checkbox-list">
-          <li
-            v-for="(ingredient, index) in selectedRecipe.ingredients"
-            :key="index"
-          >
+          <li v-for="(ingredient, index) in selectedRecipe.ingredients" :key="index">
             <!--using likeExists to remove checkbox-->
             <input
               v-if="!likeExists"
@@ -151,9 +125,7 @@
         v-if="user && user.uid === selectedRecipe.user_id && likeExists"
       >
         <button class="edit-recipe-button" @click="edit">Edit Recipe</button>
-        <button class="delete-recipe-button" @click="confirmDelete">
-          Delete Recipe
-        </button>
+        <button class="delete-recipe-button" @click="confirmDelete">Delete Recipe</button>
       </div>
     </div>
   </div>
@@ -164,6 +136,7 @@
 import RecipeImage from "./RecipeImage.vue";
 import LikeButton from "./LikeButton.vue";
 import ConfirmToast from "@/components/ConfirmToast.vue";
+import store from "@/store";
 
 import { auth, app, db, storage, fetchCategories } from "../firebase.js";
 import {
@@ -223,7 +196,7 @@ export default {
   },
   mounted() {
     this.fetchRecipeUsernameAndCookingTime();
-    console.log(this.$store.state.RecipeFromPage);
+    console.log(store.state.RecipeFromPage);
   },
   methods: {
     async fetchRecipeUsernameAndCookingTime() {
@@ -283,9 +256,7 @@ export default {
         await deleteDoc(recipeRef);
 
         console.log("Fetching categories");
-        const categoryDocsSnapshot = await getDocs(
-          collection(db, "categories")
-        );
+        const categoryDocsSnapshot = await getDocs(collection(db, "categories"));
         categoryDocsSnapshot.forEach((doc) => {
           const category = doc.data();
           if (this.selectedRecipe.categories.includes(category.category_name)) {
